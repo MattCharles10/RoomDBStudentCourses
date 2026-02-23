d74b89948125cba4ec2ab53b10670d8dd9c3013a
Mathew <matthewcharles.tech@gmail.com>
2026-02-23 11:51:44 +0530

    Splash_Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,9 @@ dependencies {
     // RecyclerView
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
+    // Splash Screen API (for Android 12+)
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
